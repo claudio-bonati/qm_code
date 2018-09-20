0a9317d3a2757fd17a0bc163c330dda5cf3d2b3f
Claudio Bonati <claudio.bonati82@gmail.com>
2018-09-20 11:18:49 +0200

    middle shooting added to shoot_numerov_d.py

--- shoot_numerov_d.py
+++ shoot_numerov_d.py
@@ -6,7 +6,8 @@ from scipy import linalg
 from scipy import optimize
 import sys
 
-__all__ = ["shooting_solver_forw_numerov", "shooting_numerov_d"]
+__all__ = ["shooting_solver_forw_numerov", "shooting_solver_back_numerov", "shooting_numerov_d", "middle_shooting_numerov_d"]
+
 
 #***************************
 #library functions
@@ -23,7 +24,7 @@ def shooting_solver_forw_numerov(potential, xmin, xmax, N, E, y0, y0_prime):
   N = number of steps in which to divide [xmin, xmax]
   E = energy guess
 
-  return the value of psi(x) in x=xmax
+  return the value of psi(x) and psi'(x) in x=xmax
   """
 
   if not isinstance(N, int):
@@ -51,14 +52,90 @@ def shooting_solver_forw_numerov(potential, xmin, xmax, N, E, y0, y0_prime):
   num = y0*(1-V(xmin+2*step)*pow(step,2)/24) +step*y0_prime*(1-V(xmin+2*step)*pow(step,2)/12) +pow(step,2)*7*y0*V(xmin)/24.0 -pow(step, 4)*V(xmin+2*step)*y0*V(xmin)/36.0
   den = 1 -V(xmin+step)*pow(step,2)/4 + V(xmin+step)*V(xmin+2*step)*pow(step, 4)/18.0
   psi_i=num/den
+
+  deriv=0.0
  
   for i in range(1, N, 1):
     psi_ip1 = (2*psi_i -psi_im1 +(10*V(xmin+i*step)*psi_i +V(xmin+(i-1)*step)*psi_im1)*pow(step,2)/12 ) / (1 - V(xmin+(i+1)*step)*pow(step,2)/12)
 
+    if i==N-1:
+     deriv+=25*psi_ip1/12
+    if i==N-2:
+     deriv+=-4*psi_ip1
+    if i==N-3:
+     deriv+=3*psi_ip1
+    if i==N-4:
+     deriv+=-4*psi_ip1/3
+    if i==N-5:
+     deriv+=psi_ip1/4
+
     psi_im1=psi_i 
     psi_i=psi_ip1
 
-  return psi_i 
+  return psi_i, deriv/step 
+
+
+def shooting_solver_back_numerov(potential, xmin, xmax, N, E, y0, y0_prime):
+  """Solver of the shooting equation (discretized using the numerov method) 
+  with initial condition 
+  
+  psi(xmax)=y0
+  psi'(xmax)=y0_prime
+
+  potential = potential energy to be used
+  the problem is defined on the interval [xmin, xmax]
+  N = number of steps in which to divide [xmin, xmax]
+  E = energy guess
+
+  return the value of psi(x) and psi'(x) in x=xmin
+  """
+
+  if not isinstance(N, int):
+    print("ERROR: N has to be an integer!")
+    sys.exit(1)
+
+  if N<2:
+    print("ERROR: N need to be at least 2!")
+    sys.exit(1)
+
+  if xmin>xmax:
+    print("ERROR: xmax has to be larger than xmin!")
+    sys.exit(1)
+
+  step=(xmax-xmin)/N
+
+  def V(x):
+    return potential(x)-E
+
+  # see Quiroz Gonzalez and Thompson "Getting started with Numerov's method" COMPUTERS IN PHYSICS, VOL. 11, 514 (1997)
+  # DOI: 10.1063/1.168593 
+
+  psi_im1=y0
+
+  num = y0*(1-V(xmax-2*step)*pow(step,2)/24) -step*y0_prime*(1-V(xmax-2*step)*pow(step,2)/12) +pow(step,2)*7*y0*V(xmax)/24.0 -pow(step, 4)*V(xmax-2*step)*y0*V(xmax)/36.0
+  den = 1 -V(xmax-step)*pow(step,2)/4 + V(xmax-step)*V(xmax-2*step)*pow(step, 4)/18.0
+  psi_i=num/den
+
+  deriv=0.0
+ 
+  for i in range(1, N, 1):
+    psi_ip1 = (2*psi_i -psi_im1 +(10*V(xmax-i*step)*psi_i +V(xmax-(i-1)*step)*psi_im1)*pow(step,2)/12 ) / (1 - V(xmax-(i+1)*step)*pow(step,2)/12)
+
+    if i==N-1:
+     deriv+=-25*psi_ip1/12
+    if i==N-2:
+     deriv+=+4*psi_ip1
+    if i==N-3:
+     deriv+=-3*psi_ip1
+    if i==N-4:
+     deriv+=+4*psi_ip1/3
+    if i==N-5:
+     deriv+=-psi_ip1/4
+
+    psi_im1=psi_i 
+    psi_i=psi_ip1
+
+  return psi_i, deriv/step 
 
 
 
@@ -77,7 +154,8 @@ def shooting_numerov_d(potential, xmin, xmax, initial_N, initial_E, tolerance, m
   locN=initial_N
   
   def f(x):
-    return shooting_solver_forw_numerov(potential, xmin, xmax, locN, x, 0, 1.0e-8)
+    ris, risprime = shooting_solver_forw_numerov(potential, xmin, xmax, locN, x, 0, 1.0e-8)
+    return ris
 
   #initial value
   ris0=optimize.newton(f, x0=initial_E, tol=tolerance)
@@ -87,7 +165,7 @@ def shooting_numerov_d(potential, xmin, xmax, initial_N, initial_E, tolerance, m
 
   while delta>tolerance and iteration < maxiter:
     locN*=2 
-    ris1=optimize.newton(f, x0=initial_E, tol=tolerance, maxiter=500)
+    ris1=optimize.newton(f, x0=ris0, tol=tolerance, maxiter=500)
     delta=abs(ris1-ris0)
     ris0=ris1
     iteration+=1
@@ -97,6 +175,48 @@ def shooting_numerov_d(potential, xmin, xmax, initial_N, initial_E, tolerance, m
     print("") 
 
   return ris0, locN
+
+
+
+def middle_shooting_numerov_d(potential, xmin, xmax, initial_N, initial_E, tolerance, maxiter=500):
+  """Shooting in the middle using numerov discretization with Dirichlet boundary conditions.
+
+  potential = potential energy to be used
+  the problem is defined on the interval [xmin, xmax]
+  initial_N = initial number of steps to be used
+  initial_E = initial estimate for the eigenvalue
+  tolerange = precision goal
+
+  return the energy and the final value of steps used
+  """
+
+  locN=initial_N
+  
+  def f(x):
+    r1, rp1 = shooting_solver_forw_numerov(potential, xmin, (xmax+xmin)/2.0, locN, x[0], 0.0, 1.0e-8)
+    r2, rp2 = shooting_solver_back_numerov(potential, (xmax+xmin)/2.0, xmax, locN, x[0], 0.0, x[1])
+    return np.array([r1-r2, rp1-rp2])
+
+  #initial value
+  ris0=optimize.root(f, x0=np.array([initial_E, -1.0e-8]), tol=tolerance)
+  
+  delta=1.0
+  iteration=1
+
+  while delta>tolerance and iteration < maxiter:
+    locN*=2 
+    ris1=optimize.root(f, x0=ris0.x, tol=tolerance)
+    delta=abs(ris1.x[0]-ris0.x[0])
+    ris0=ris1
+    iteration+=1
+
+  if(iteration==maxiter):
+    print("WARNING: maximum number of iterations reached")
+    print("") 
+
+  return ris0.x[0], locN
+
+
 
 
 #***************************
@@ -127,7 +247,7 @@ if __name__=="__main__":
   print('')
   print('{:>2s} {:>15s} {:>15s} {:>15s} {:>15s}'.format("n", "fd_3p_d", "shooting", "steps", "exact"))
   for i in range(0, 10, 2):
-     ris, finalN=shooting_numerov_d(pot_h, xmin, xmax, N, risA[i], goal)
+     ris, finalN=middle_shooting_numerov_d(pot_h, xmin, xmax, N, risA[i], goal)
      print('{:>2d} {:>15.10f} {:>15.10f} {:>15d} {:>15.10f}'.format(i, risA[i], ris, finalN, 2*i+1))
   
   print("")
@@ -198,7 +318,7 @@ if __name__=="__main__":
   print('')
   print('{:>2s} {:>15s} {:>15s} {:>15s} {:>15s}'.format("n", "fd_3p_d", "shooting", "steps", "known"))
   for i in range(0, 10, 2):
-     ris, finalN=shooting_numerov_d(pot_p, xmin, xmax, 10000, risA[i], goal)
+     ris, finalN = middle_shooting_numerov_d(pot_p, xmin, xmax, 10000, risA[i], goal)
      print('{:>2d} {:>15.10f} {:>15.10f} {:>15d} {:>15.10f}'.format(i, risA[i], ris, finalN, exact_p(i)))
 
 
@@ -216,7 +336,7 @@ if __name__=="__main__":
 
   print("{:>5s} {:>15s}".format("N", "err*N^4"))
   for N in range(20, 210, 20):
-    ris = shooting_solver_forw_numerov(pot_l, 0, 1, N, 1.5, 0.2, 1.0)
+    ris, risprime = shooting_solver_forw_numerov(pot_l, 0, 1, N, 1.5, 0.2, 1.0)
     print("{:>5d} {:>15.10f}".format(N, (exact_ris-ris)*pow(N,4)))
 
   print("**********************")
