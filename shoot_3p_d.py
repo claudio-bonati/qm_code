556d3f704b131b5bfb1375d5676e82c73f50f49c
Claudio Bonati <claudio.bonati82@gmail.com>
2018-09-20 10:45:47 +0200

    shooting in the middle added to shoot_3p_d.py

--- shoot_3p_d.py
+++ shoot_3p_d.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy import optimize
 import sys
 
-__all__ = ["shooting_solver_forw_3p", "shooting_3p_d"]
+__all__ = ["shooting_solver_forw_3p", "shooting_solver_back_3p", "shooting_3p_d", "middle_shooting_3p_d"]
 
 #***************************
 #library functions
@@ -22,7 +22,7 @@ def shooting_solver_forw_3p(potential, xmin, xmax, N, E, y0, y0_prime):
   N = number of steps in which to divide [xmin, xmax]
   E = energy guess
 
-  return the value of psi(x) in x=xmax
+  return the value of psi(x) and psi'(x) in x=xmax
   """
 
   if not isinstance(N, int):
@@ -45,10 +45,59 @@ def shooting_solver_forw_3p(potential, xmin, xmax, N, E, y0, y0_prime):
 
   for i in range(1, N, 1):
     psi_ip1=2*psi_i - psi_im1 - step*step*(E*psi_i-potential(xmin+i*step)*psi_i)
+
+    if i==N-1:
+     deriv=3*psi_ip1/2-2*psi_i+psi_im1/2
+
     psi_im1=psi_i 
     psi_i=psi_ip1
 
-  return psi_i 
+  return psi_i, deriv/step 
+
+
+def shooting_solver_back_3p(potential, xmin, xmax, N, E, y0, y0_prime):
+  """Solver of the shooting equation (discretized using a 3 point discretization
+  of the second derivative) with initial condition 
+  
+  psi(xmax)=y0
+  psi'(xmax)=y0_prime
+
+  potential = potential energy to be used
+  the problem is defined on the interval [xmin, xmax]
+  N = number of steps in which to divide [xmin, xmax]
+  E = energy guess
+
+  return the value of psi(x) and psi'(x) in x=xmin
+  """
+
+  if not isinstance(N, int):
+    print("ERROR: N has to be an integer!")
+    sys.exit(1)
+
+  if N<2:
+    print("ERROR: N need to be at least 2!")
+    sys.exit(1)
+
+  if xmin>xmax:
+    print("ERROR: xmax has to be larger than xmin!")
+    sys.exit(1)
+
+  step=(xmax-xmin)/N
+
+  # starting values at xmin
+  psi_im1=y0
+  psi_i=y0-y0_prime*step+(potential(xmax)-E)*y0*pow(step,2)/2
+
+  for i in range(1, N, 1):
+    psi_ip1=2*psi_i - psi_im1 - step*step*(E*psi_i-potential(xmax-i*step)*psi_i)
+
+    if i==N-1:
+     deriv=-3*psi_ip1/2+2*psi_i-psi_im1/2
+
+    psi_im1=psi_i 
+    psi_i=psi_ip1
+
+  return psi_i, deriv/step 
 
 
 
@@ -67,7 +116,8 @@ def shooting_3p_d(potential, xmin, xmax, initial_N, initial_E, tolerance, maxite
   locN=initial_N
   
   def f(x):
-    return shooting_solver_forw_3p(potential, xmin, xmax, locN, x, 0.0, 1.0e-8)
+    ris, risprime = shooting_solver_forw_3p(potential, xmin, xmax, locN, x, 0.0, 1.0e-8)
+    return ris
 
   #initial value
   ris0=optimize.newton(f, x0=initial_E, tol=tolerance)
@@ -77,7 +127,7 @@ def shooting_3p_d(potential, xmin, xmax, initial_N, initial_E, tolerance, maxite
 
   while delta>tolerance and iteration < maxiter:
     locN*=2 
-    ris1=optimize.newton(f, x0=initial_E, tol=tolerance, maxiter=500)
+    ris1=optimize.newton(f, x0=ris0, tol=tolerance, maxiter=500)
     delta=abs(ris1-ris0)
     ris0=ris1
     iteration+=1
@@ -87,6 +137,46 @@ def shooting_3p_d(potential, xmin, xmax, initial_N, initial_E, tolerance, maxite
     print("") 
 
   return ris0, locN
+
+
+def middle_shooting_3p_d(potential, xmin, xmax, initial_N, initial_E, tolerance, maxiter=500):
+  """Shooting in the middle using 3 points discretization with Dirichlet boundary conditions.
+
+  potential = potential energy to be used
+  the problem is defined on the interval [xmin, xmax]
+  initial_N = initial number of steps to be used
+  initial_E = initial estimate for the eigenvalue
+  tolerange = precision goal
+
+  return the energy and the final value of steps used
+  """
+
+  locN=initial_N
+  
+  def f(x):
+    r1, rp1 = shooting_solver_forw_3p(potential, xmin, (xmax+xmin)/2.0, locN, x[0], 0.0, 1.0e-8)
+    r2, rp2 = shooting_solver_back_3p(potential, (xmax+xmin)/2.0, xmax, locN, x[0], 0.0, x[1])
+    return np.array([r1-r2, rp1-rp2])
+
+  #initial value
+  ris0=optimize.root(f, x0=np.array([initial_E, -1.0e-8]), tol=tolerance)
+  
+  delta=1.0
+  iteration=1
+
+  while delta>tolerance and iteration < maxiter:
+    locN*=2 
+    ris1=optimize.root(f, x0=ris0.x, tol=tolerance)
+    delta=abs(ris1.x[0]-ris0.x[0])
+    ris0=ris1
+    iteration+=1
+
+  if(iteration==maxiter):
+    print("WARNING: maximum number of iterations reached")
+    print("") 
+
+  return ris0.x[0], locN
+
 
 
 #***************************
@@ -117,7 +207,7 @@ if __name__=="__main__":
   print('')
   print('{:>2s} {:>15s} {:>15s} {:>15s} {:>15s}'.format("n", "fd_3p_d", "shooting", "steps", "exact"))
   for i in range(0, 10, 2):
-     ris, finalN=shooting_3p_d(pot_h, xmin, xmax, N, risA[i], goal)
+     ris, finalN=middle_shooting_3p_d(pot_h, xmin, xmax, N, risA[i], goal)
      print('{:>2d} {:>15.10f} {:>15.10f} {:>15d} {:>15.10f}'.format(i, risA[i], ris, finalN, 2*i+1))
   
   print("")
@@ -188,7 +278,7 @@ if __name__=="__main__":
   print('')
   print('{:>2s} {:>15s} {:>15s} {:>15s} {:>15s}'.format("n", "fd_3p_d", "shooting", "steps", "known"))
   for i in range(0, 10, 2):
-     ris, finalN=shooting_3p_d(pot_p, xmin, xmax, 10000, risA[i], goal)
+     ris, finalN=middle_shooting_3p_d(pot_p, xmin, xmax, 10000, risA[i], goal)
      print('{:>2d} {:>15.10f} {:>15.10f} {:>15d} {:>15.10f}'.format(i, risA[i], ris, finalN, exact_p(i)))
 
   print("")
@@ -206,7 +296,7 @@ if __name__=="__main__":
 
   print("{:>5s} {:>15s}".format("N", "err*N^2"))
   for N in range(20, 210, 20):
-    ris = shooting_solver_forw_3p(pot_l, 0, 1, N, 1.5, 0.2, 1.0)
+    ris, risprime = shooting_solver_forw_3p(pot_l, 0, 1, N, 1.5, 0.2, 1.0)
     print("{:>5d} {:>15.10f}".format(N, (exact_ris-ris)*pow(N,2)))
 
   print("**********************")
